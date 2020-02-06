730d044b55cdcc3167cefd8ea51983c22f690a63
Nick Capps <nickcapps10@gmail.com>
2020-02-05 17:20:11 -0800

    Add closure examples

--- src/scope-closures.js
+++ src/scope-closures.js
@@ -1,4 +1,5 @@
 import _ from 'underscore';
+import { average } from './first-class-fun';
 
 export function whatWasTheLocal() {
   const captured = 'Oh hai';
@@ -9,5 +10,40 @@ export function whatWasTheLocal() {
 }
 
 export function createScaleFunction(factor) {
-  return
+  return function (v) {
+    return _.map(v, (n) => n * factor);
+  };
 }
+
+export function makeAdder(captured) {
+  return function (free) {
+    return free + captured;
+  };
+}
+
+export function averageDamp(fun) {
+  return function (n) {
+    return average([n, fun(n)]);
+  };
+}
+
+export function showObject(obj) {
+  return function () {
+    return obj;
+  };
+}
+
+export const pingpong = (function pingpong() {
+  let pri = 0;
+
+  return {
+    inc(n) {
+      pri += n;
+      return pri;
+    },
+    dec(n) {
+      pri -= n;
+      return pri;
+    },
+  };
+}());
--- test/scope-closures.test.js
+++ test/scope-closures.test.js
@@ -5,4 +5,36 @@ describe('closure examples', () => {
     const reportLocal = sc.whatWasTheLocal();
     expect(reportLocal()).toBe('The local was: Oh hai');
   });
+
+  test('should scale', () => {
+    const scale10 = sc.createScaleFunction(10);
+    expect(scale10([1, 2, 3])).toEqual([10, 20, 30]);
+  });
+
+  test('should adder', () => {
+    const add10 = sc.makeAdder(10);
+    const add1024 = sc.makeAdder(1024);
+    expect(add10(32)).toBe(42);
+    expect(add10(98)).toBe(108);
+    expect(add1024(11)).toBe(1035);
+  });
+
+  test('should averageDampe', () => {
+    const averageSq = sc.averageDamp((n) => n * n);
+    expect(averageSq(10)).toBe(55);
+  });
+
+  test('should showObject', () => {
+    const o = { a: 42 };
+    const showO = sc.showObject(o);
+    expect(showO()).toEqual({ a: 42 });
+    o.newField = 108;
+    expect(showO()).toEqual({ a: 42, newField: 108 });
+  });
+
+  test('should pingpong', () => {
+    expect(sc.pingpong.inc(10)).toBe(10);
+    expect(sc.pingpong.inc(10)).toBe(20);
+    expect(sc.pingpong.dec(7)).toBe(13);
+  });
 });
